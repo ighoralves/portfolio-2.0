06eaa1d6531c1b94e010c4d1a6a0be7832c84144
Ighor Alves <ighoralc@gmail.com>
2023-08-23 01:07:37 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './styles/app.scss';
+
+function App() {
+  return (
+    <>
+      <div className="Hello-text">
+        Hello mundo
+
+        <div className="Hello-mundo">
+
+          Olá Mundo!!
+
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
